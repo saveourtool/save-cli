3de6fdc3b9e163dfe6b422bf1597e5184b0982c3
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-18 11:26:27 +0300

    Repository infrastructure
    ### What's done:
    * Added diktat and detekt configuration
    * Added github workflows
    * Added git hooks
    * Refactored gradle configs
    * Added badges

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.cqfn.diktat:diktat-gradle-plugin:0.4.2")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.15.0")
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,3 @@
+object Versions {
+    val kotlin = "1.4.31"
+}
