7b6f2b13eefb85b780de27904361b310cd868ed9
Peter Trifanov <peter.trifanov@mail.ru>
2021-03-22 18:19:12 +0300

    Describe usage scenarios of SAVE
    ### What's done:
    * Changed native target name for libraries

--- save-plugins/warnings-plugin/build.gradle.kts
+++ save-plugins/warnings-plugin/build.gradle.kts
@@ -12,9 +12,9 @@ kotlin {
     jvm()
     val os = getCurrentOperatingSystem()
     val saveTarget = when {
-        os.isMacOsX -> macosX64("save")
-        os.isLinux -> linuxX64("save")
-        os.isWindows -> mingwX64("save")
+        os.isMacOsX -> macosX64()
+        os.isLinux -> linuxX64()
+        os.isWindows -> mingwX64()
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
