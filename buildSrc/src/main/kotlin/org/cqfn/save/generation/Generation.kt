a31632a16978e83046bab0fe20443eec7f03d646
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-04-12 16:57:37 +0300

    Generate SaveConfig.kt ### What's done: * Implement generation of SaveConfig.kt

--- buildSrc/src/main/kotlin/org/cqfn/save/generation/Generation.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/generation/Generation.kt
@@ -4,9 +4,16 @@
 
 package org.cqfn.save.generation
 
-import com.squareup.kotlinpoet.FileSpec
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.TypeSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.AnnotationSpec
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.TypeName
+import com.squareup.kotlinpoet.KModifier
 
 import java.io.File
 import java.io.BufferedReader
@@ -20,11 +27,12 @@ private val autoGenerationComment =
     """.trimMargin()
 
 class Option {
-    var type: String? = null
-    var fullName: String? = null
-    var shortName: String? = null
-    var description: String?= null
-    var option: Map<String, String>? = null
+    lateinit var argType: String
+    lateinit var kotlinType: String
+    lateinit var fullName: String
+    lateinit var shortName: String
+    lateinit var description: String
+    lateinit var option: Map<String, String>
 }
 
 fun main() {
@@ -44,24 +52,80 @@ fun main() {
 
 fun generateConfig(jsonObject: Map<String, Option>) {
     jsonObject.forEach {
-        val currOption = it.value
-        println("${it.key} ${currOption.type}")
+        //val currOption = it.value
+        //println("${it.key} ${currOption.type}")
     }
+    /*
+    val t = FunSpec.builder()
+        .addCode("""
+            |private fun <U> Map<String, String>.getAndParseOrElse(
+    |key: String,
+    |parse: String.() -> U,
+    |default: () -> U) =
+        |get(key)?.let(parse) ?: default()
+            """)
 
-    /* TODO
-    val kotlinFile = FileSpec
-        .builder("org.cqfn.save.cli", "Config")
-        .addType(fileBody)
+    val kotlinFile = FileSpec.builder("org.cqfn.save.cli", "Config")
+        //.addType(fileBody)
         .indent("    ")
         .addComment(autoGenerationComment)
+        .addFunction(FunSpec.builder("main")
+            .addStatement("val a = %M()", createA)
+            .addStatement("val b = %M()", createB)
+            .addStatement("println(a.%M)")
+            .addStatement("println(b.%M)")
+            .build())
         .build()
+    */
 
-     */
+    //kotlinFile.writeTo(System.out)
 }
 
 fun generateSaveConfig(jsonObject: Map<String, Option>) {
+    println("-----------generateSaveConfig------------")
+    val builder = FileSpec.builder("org.cqfn.save.core.config", "SaveConfig")
+    builder.addImport("okio", "ExperimentalFileSystem")
 
+    var properties = ""
+    jsonObject.forEach { properties += ("@property ${it.key} ${it.value.description}\n")}
+    val kdoc = """
+               |Configuration properties of save application, retrieved either from properties file
+               |or from CLI args.
+               |$properties
+               """.trimMargin()
+
+    val classBuilder = TypeSpec.classBuilder("SaveConfig").addModifiers(KModifier.DATA)
+    classBuilder.addKdoc(kdoc)
+    val experimentalFileSystem = AnnotationSpec.builder(ClassName("kotlin", "OptIn")).addMember("ExperimentalFileSystem::class")
+
+    classBuilder.addAnnotation(experimentalFileSystem.build())
+
+    val ctor = FunSpec.constructorBuilder()
+
+    for ((name, value) in jsonObject) {
+        ctor.addParameter(name, selectType(value))
+        val property = PropertySpec.builder(name, selectType(value)).initializer(name).build()
+        classBuilder.addProperty(property)
+    }
+    classBuilder.primaryConstructor(ctor.build())
+
+    builder.addType(classBuilder.build())
+    builder.build().writeTo(System.out)
+    println("-------------------------------------")
 }
+
+fun selectType(value: Option): TypeName =
+    when(value.kotlinType) {
+        "Boolean" -> ClassName("kotlin", "Boolean")
+        "Int" -> ClassName("kotlin", "Int")
+        "String" -> ClassName("kotlin", "String")
+        "Path" -> ClassName("okio", "Path")
+        "Path?" -> ClassName("okio", "Path").copy(nullable = true)
+        "ReportType" -> ClassName("org.cqfn.save.core.config","ReportType")
+        "LanguageType" -> ClassName("org.cqfn.save.core.config","LanguageType")
+        "ResultOutputType" -> ClassName("org.cqfn.save.core.config","ResultOutputType")
+        else -> ClassName("kotlin", "Unit")
+    }
 
 fun generateReadme(jsonObject: Map<String, Option>) {
 
