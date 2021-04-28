8f5712a4a870cd4587c67ea8864e30152daac493
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-04-28 16:19:20 +0300

    Fix redirections ### What's done: * Fix redirections

--- save-plugins/fix-plugin/src/commonTest/kotlin/org/cqfn/save/plugins/fix/FixPluginTest.kt
+++ save-plugins/fix-plugin/src/commonTest/kotlin/org/cqfn/save/plugins/fix/FixPluginTest.kt
@@ -98,7 +98,7 @@ class FixPluginTest {
                 write("#!/bin/bash\n".encodeToByteArray())
             }
             write("cd $tmpDir\n".encodeToByteArray())
-            write("echo Expected file> Test3Test.java".encodeToByteArray())
+            write("echo Expected file > Test3Test.java".encodeToByteArray())
         }
         val results = FixPlugin().execute(
             mockConfig,
@@ -141,7 +141,7 @@ class FixPluginTest {
                 write("#!/bin/bash\n".encodeToByteArray())
             }
             write("cd $tmpDir\n".encodeToByteArray())
-            write("echo Expected file> Test3Test_copy.java".encodeToByteArray())
+            write("echo Expected file > Test3Test_copy.java".encodeToByteArray())
         }
         val results = FixPlugin().execute(
             mockConfig,
