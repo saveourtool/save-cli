f6245da829936a6c31fb24004bd085070183fbfb
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-12 12:49:17 +0300

    Fixed detekt task
    ### What's done:
    * Code style

--- save-plugins/warn-plugin/src/commonMain/kotlin/org/cqfn/save/plugin/warn/utils/Warning.kt
+++ save-plugins/warn-plugin/src/commonMain/kotlin/org/cqfn/save/plugin/warn/utils/Warning.kt
@@ -29,7 +29,10 @@ data class Warning(
  * @return a [Warning] or null if [this] string doesn't match [warningRegex]
  * @throws ResourceFormatException when parsing a file
  */
-@Suppress("TooGenericExceptionCaught", "SwallowedException", "ThrowsCount")
+@Suppress(
+    "TooGenericExceptionCaught",
+    "SwallowedException",
+    "ThrowsCount")
 internal fun String.extractWarning(warningRegex: Regex,
                                    columnGroupIdx: Int?,
                                    lineGroupIdx: Int?,
