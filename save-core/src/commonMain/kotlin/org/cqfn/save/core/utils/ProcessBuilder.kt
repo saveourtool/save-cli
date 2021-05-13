382b2fdd5ab3e5da1ada4b0c7f8720aaf75b1fd3
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-05-13 17:40:17 +0300

    (WIP) Handle situation, when extra whitespace created if case of usage echo in Windows ### What's done: * For on processing user commands with echo * Add tests

--- save-core/src/commonMain/kotlin/org/cqfn/save/core/utils/ProcessBuilder.kt
+++ save-core/src/commonMain/kotlin/org/cqfn/save/core/utils/ProcessBuilder.kt
@@ -15,6 +15,7 @@ import okio.Path
 import okio.Path.Companion.toPath
 
 import kotlinx.datetime.Clock
+import kotlin.math.min
 
 /**
  * A class that is capable of executing processes, specific to different OS and returning their output.
@@ -87,6 +88,10 @@ class ProcessBuilder {
             logWarn("Found user provided redirections in `$command`. " +
                     "SAVE uses own redirections for internal purpose and will redirect all streams to $tmpDir")
         }
+        if (isCurrentOsWindows()) {
+            // TODO use return value
+            processCommandWithEcho(command)
+        }
         val cmd = processBuilderInternal.prepareCmd(command)
         logDebug("Executing: $cmd")
         val status = try {
@@ -108,6 +113,66 @@ class ProcessBuilder {
             }
         } ?: logDebug("Execution output:\n$stdout")
         return ExecutionResult(status, stdout, stderr)
+    }
+
+    companion object {
+        fun processCommandWithEcho(command: String): String {
+            if (!command.contains("echo")) {
+                return command
+            }
+            // Command already contains correct signature.
+            // We also believe not to met complex cases: `echo a; echo | set /p="a && echo b"`,
+            // when there is additionally exists `echo` without `set /p`,
+            // we suppose, that when user use `set /p` he knew what he did
+            val cmdWithoutWhitespaces = command.replace(" ", "")
+            if (cmdWithoutWhitespaces.contains("echo|set")) {
+                return command
+            }
+            if (cmdWithoutWhitespaces.contains("echo\"")) {
+                logWarn("Please use echo | set /p\"your command\" to avoid extra whitespaces on Windows")
+                return command
+            }
+            // If command is complex, we need to modify only `echo` subcommands
+            val separator = if (command.contains("&&")) "&&" else if (command.contains(";")) ";" else ""
+            val listOfCommands = if (separator != "") command.split(separator) as MutableList<String> else mutableListOf(command)
+            println("Before ${listOfCommands}")
+            println(command)
+            listOfCommands.forEachIndexed { index, cmd ->
+                println("CMD: ${cmd}")
+                if (cmd.contains("echo")) {
+                    var newEchoCommand = cmd.trim(' ').replace("echo ", " echo | set /p=\"")
+                    // Despite the fact, that we don't expect user redirections, for out internal tests we use them,
+                    // so we need to process such cases
+                    // There three different cases, when we need to insert closing `"`.
+                    // 1) Before stdout redirection
+                    // 2) Before stderr redirection
+                    // 3) At the end of string, if there is no redirections
+                    val indexOfStdoutRedirection = if (newEchoCommand.indexOf(">") != -1) newEchoCommand.indexOf(">") else newEchoCommand.length
+                    val indexOfStderrRedirection = if (newEchoCommand.indexOf("2>") != -1) newEchoCommand.indexOf("2>") else newEchoCommand.length
+                    val insertIndex = minOf(indexOfStdoutRedirection, indexOfStderrRedirection)//, newEchoCommand.length - 1)
+                    //println("${newEchoCommand}|")
+                    //println(" indexOfStdoutRedirection ${indexOfStdoutRedirection}")
+                    //println(" indexOfStderrRedirection ${indexOfStderrRedirection}")
+                    //println(" insertIndex ${insertIndex}")
+                    println(" Substring before ${newEchoCommand.substring(0, insertIndex)}|")
+                    println(" Substring after ${newEchoCommand.substring(insertIndex, newEchoCommand.length)}|")
+                    newEchoCommand = newEchoCommand.substring(0, insertIndex).trimEnd(' ') + "\" " +  newEchoCommand.substring(insertIndex, newEchoCommand.length) + " "
+
+                    listOfCommands[index] = newEchoCommand
+                }
+            }
+            val modifiedCommand = listOfCommands.joinToString(separator).trim(' ')
+            println("After ${listOfCommands}")
+            println(modifiedCommand)
+            return modifiedCommand
+        }
+
+        fun extractEchoCommands(command: String, separator: String): MutableList<String> {
+            val echoCommands: MutableList<String> = mutableListOf()
+            val listOfCommands = command.split(separator).filter { it.contains("echo") }
+            listOfCommands.forEach { echoCommands.add(it.trim(' ')) }
+            return echoCommands
+        }
     }
 
     /**
--- save-core/src/commonTest/kotlin/org/cqfn/save/core/ProcessBuilderTest.kt
+++ save-core/src/commonTest/kotlin/org/cqfn/save/core/ProcessBuilderTest.kt
@@ -5,6 +5,7 @@
 package org.cqfn.save.core
 
 import org.cqfn.save.core.utils.ProcessBuilder
+import org.cqfn.save.core.utils.ProcessBuilder.Companion.processCommandWithEcho
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
@@ -30,5 +31,40 @@ class ProcessBuilderTest {
         // posix `system()` and JVM process builder returns lines with different whitespaces, so we cut them
         assertEquals(expectedStdout, actualResult.stdout[0].trimEnd())
         assertEquals(expectedStderr, actualResult.stderr)
+    }
+
+    @Test
+    fun `simple check`() {
+        val inputCommand = "echo something"
+        val expectedCommand = "echo | set /p=\"something\""
+        assertEquals(expectedCommand, processCommandWithEcho(inputCommand))
+    }
+
+    @Test
+    fun `simple check with redirection`() {
+        val inputCommand = "echo something > /dev/null"
+        val expectedCommand = "echo | set /p=\"something\" > /dev/null"
+        assertEquals(expectedCommand, processCommandWithEcho(inputCommand))
+    }
+
+    @Test
+    fun `simple check with redirection without first whitespace`() {
+        val inputCommand = "echo something> /dev/null"
+        val expectedCommand = "echo | set /p=\"something\" > /dev/null"
+        assertEquals(expectedCommand, processCommandWithEcho(inputCommand))
+    }
+
+    @Test
+    fun `simple check with redirection without whitespaces at all`() {
+        val inputCommand = "echo something>/dev/null"
+        val expectedCommand = "echo | set /p=\"something\" >/dev/null"
+        assertEquals(expectedCommand, processCommandWithEcho(inputCommand))
+    }
+
+    @Test
+    fun `change multiple echo commands with redirections`() {
+        val inputCommand = "echo a > /dev/null && echo b 2>/dev/null && ls"
+        val expectedCommand = "echo | set /p=\"a\" > /dev/null && echo | set /p=\"b\" 2>/dev/null && ls"
+        assertEquals(expectedCommand, processCommandWithEcho(inputCommand))
     }
 }
