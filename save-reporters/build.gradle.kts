6e1404534673366601ef347b7840faf84e1411e6
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-14 17:33:39 +0300

    Reporter API
    ### What's done:
    * Fixes after merge

--- save-reporters/build.gradle.kts
+++ save-reporters/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(project(":save-core"))
+                implementation(project(":save-common"))
             }
         }
     }
