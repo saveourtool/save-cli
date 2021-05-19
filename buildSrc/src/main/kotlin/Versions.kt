cb75f78a1b187612069617c6917d1840bcc5262b
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-19 17:38:37 +0300

    Version updates
    ### What's done:
    * Koltin 1.5
    * Others

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 @file:Suppress("PACKAGE_NAME_MISSING", "CONSTANT_UPPERCASE")
 
 object Versions {
-    const val kotlin = "1.4.32"
-    const val junit = "5.7.1"
+    const val kotlin = "1.5.0"
+    const val junit = "5.7.2"
     const val okio = "3.0.0-alpha.1"
     const val ktoml = "0.2.1"
 
