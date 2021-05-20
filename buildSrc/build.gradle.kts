60e3e9af2d37893c8125d48d3438d28245675618
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-20 16:21:49 +0300

    Change reckon plugin to git-version plugin
    ### What's done:
    * Changed plugin
    * Added release property

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.16.0")
     runtimeOnly(kotlin("gradle-plugin", "1.5.0"))
     implementation("io.github.gradle-nexus:publish-plugin:1.1.0")
-    implementation("org.ajoberstar.reckon:reckon-gradle:0.13.0")
+    implementation("com.palantir.gradle.gitversion:gradle-git-version:0.12.3")
     implementation("com.squareup:kotlinpoet:1.8.0")
     implementation("com.google.code.gson:gson:2.8.6")
 }
--- buildSrc/src/main/kotlin/org/cqfn/save/buildutils/VersioningConfiguration.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/VersioningConfiguration.kt
@@ -4,18 +4,30 @@
 
 package org.cqfn.save.buildutils
 
-import org.ajoberstar.reckon.gradle.ReckonExtension
-import org.ajoberstar.reckon.gradle.ReckonPlugin
+import com.palantir.gradle.gitversion.GitVersionPlugin
+import com.palantir.gradle.gitversion.VersionDetails
+import groovy.lang.Closure
+import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
-import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.extra
+import org.gradle.kotlin.dsl.invoke
+import org.gradle.kotlin.dsl.provideDelegate
 
-@Suppress("MISSING_KDOC_ON_FUNCTION", "MISSING_KDOC_TOP_LEVEL")
+/**
+ * Configures how project version is determined. We are using `git-version` plugin to get a version from git repo.
+ * If working tree is dirty (i.e. there are uncommitted or untracked changes) and release build is attempted, build will fail.
+ */
 fun Project.configureVersioning() {
-    apply<ReckonPlugin>()
+    apply<GitVersionPlugin>()
 
-    configure<ReckonExtension> {
-        scopeFromProp()
-        stageFromProp("alpha", "rc", "final")  // version string will be based on last commit; when checking out a tag, that tag will be used
+    val versionDetails: Closure<VersionDetails> by extra
+    val details = versionDetails.invoke()
+    logger.lifecycle("Discovered version ${details.version}, working tree is ${if (details.isCleanTag) "clean" else "dirty"}")
+
+    // to activate release, provide `-Prelease` or `-Prelease=true`. To deactivate, either omit the property, or set `-Prelease=false`.
+    val isRelease = hasProperty("release") && (property("release") as String != "false")
+    if (isRelease && !details.isCleanTag) {
+        throw GradleException("Release build will be performed with not clean git tree; aborting.")
     }
 }
