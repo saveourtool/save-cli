9f5869d5ce99eb68659f4016762f35df1e4473a2
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-05-20 16:38:39 +0300

    Add more comments to config detector ### What's done: * Add more comments to config detector

--- save-core/src/commonMain/kotlin/org/cqfn/save/core/files/ConfigDetector.kt
+++ save-core/src/commonMain/kotlin/org/cqfn/save/core/files/ConfigDetector.kt
@@ -25,20 +25,26 @@ class ConfigDetector {
         val file = testConfig!!.toPath()
         return discoverConfigWithParents(file)
             ?.also { config ->
-                // fill children for parent configs
+                // After `discoverConfigWithParents` we successfully created TestConfig instances for all save.toml files
+                // from given [testConfig] to the top-level save.toml in file tree. Also hierarchy of parents were provided
+                // Now fill children for parent configs
                 config.parentConfigs(wihSelf = true)
                     .toList()
                     .reversed()
                     .zipWithNext()
-                    .forEach { (parent, child) -> parent.childConfigs.add(child) }
+                    .forEach {
+                        (parent, child) -> parent.childConfigs.add(child)
+                        logDebug("Add child ${child.location} for ${parent.location}")
+                    }
 
+                // Now go down the file tree and
                 // discover all descendant configs of [config]
-                val locationsFlattened = config
+                val descendantConfigLocations = config
                     .directory
                     .findAllFilesMatching { it.isSaveTomlConfig() }
                     .flatten()
 
-                createTestConfigs(locationsFlattened, mutableListOf(config))
+                createTestConfigs(descendantConfigLocations, mutableListOf(config))
             }
             ?: run {
                 logError("Config file was not found in $file")
@@ -47,13 +53,13 @@ class ConfigDetector {
             }
     }
 
-    private fun createTestConfigs(locationsFlattened: List<Path>, configs: MutableList<TestConfig>) =
-            locationsFlattened
+    private fun createTestConfigs(descendantConfigLocations: List<Path>, configs: MutableList<TestConfig>) =
+        descendantConfigLocations
                 .drop(1)  // because [config] will be discovered too
                 .forEachIndexed { index, path ->
                     val parentConfig = configs.find {
                         it.location ==
-                                locationsFlattened.take(index + 1)
+                                descendantConfigLocations.take(index + 1)
                                     .reversed()
                                     .find { it.parent in path.parents() }!!
                     }!!
@@ -65,10 +71,11 @@ class ConfigDetector {
 
                     logDebug("Found config file at $path, adding as a child for ${parentConfig.location}")
                     newChildConfig.neighbourConfigs?.add(newChildConfig)
+                    //newChildConfig.parentConfig!!.childConfigs.add(newChildConfig)
                 }
 
     /**
-     * Depends to type of entry point, start to create the hierarchy of TestConfig's, from bottom to top
+     * Depends to type of entry point, start to create the hierarchy of TestConfig's, from [file] to top in the file tree
      *
      * @param file entry point from which SAVE should create hierarchy of [TestConfig]'s.
      * @return [TestConfig] or null if no suitable config file has been found.
@@ -88,7 +95,7 @@ class ConfigDetector {
         .mapNotNull { it.findChildByOrNull { it.isSaveTomlConfig() } }
         .firstOrNull()
         ?.let { discoverConfigWithParents(it) }
-        .also { logDebug("Processing test config from a single test file: $file") }
+        .also { logDebug("Processing test config for a single test file: $file") }
 
     private fun getTestConfigFromDirectory(file: Path) = file
         .findChildByOrNull { it.isSaveTomlConfig() }
@@ -96,6 +103,7 @@ class ConfigDetector {
         .also { logDebug("Processing test config from directory: $file") }
 
     private fun getTestConfigFromTomlFile(file: Path): TestConfig {
+        // Create instances for all parent configs recursively
         val parentConfig = file.parents()
             .drop(1)  // because immediate parent already contains [this] config
             .mapNotNull { parentDir ->
@@ -105,9 +113,8 @@ class ConfigDetector {
             }
             .firstOrNull()
             ?.let { getTestConfigFromTomlFile(it) }
-
+        // Now process current config
         logDebug("Processing test config from the toml file: $file")
-
         return TestConfig(
             file,
             parentConfig
