db9db42141fe57884ca986cb1b75695fa11a6685
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-20 17:55:32 +0300

    Reverted reckon back
    ### What's done:
    * Fixes

--- buildSrc/src/main/kotlin/org/cqfn/save/buildutils/VersioningConfiguration.kt
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/VersioningConfiguration.kt
@@ -6,19 +6,16 @@
 
 package org.cqfn.save.buildutils
 
+import org.ajoberstar.grgit.Grgit
 import org.ajoberstar.reckon.gradle.ReckonExtension
 import org.ajoberstar.reckon.gradle.ReckonPlugin
 import org.gradle.api.GradleException
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.ajoberstar.grgit.Grgit
-
-internal val tagPattern = Regex("""^v(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*).*$""")
 
 /**
- * Configures how project version is determined. We are using `git-version` plugin to get a version from git repo.
- * If working tree is dirty (i.e. there are uncommitted or untracked changes) and release build is attempted, build will fail.
+ * Configures how project version is determined.
  *
  * @throws GradleException if there was an attempt to run release build with dirty working tree
  */
@@ -32,9 +29,11 @@ fun Project.configureVersioning() {
 
     // to activate release, provide `-Prelease` or `-Prelease=true`. To deactivate, either omit the property, or set `-Prelease=false`.
     val isRelease = hasProperty("release") && (property("release") as String != "false")
-    val grgit: Grgit = project.findProperty("grgit") as Grgit  // grgit is added by reckon plugin
-    val isClean = grgit.repository.jgit.status().call().isClean
-    if (isRelease && !isClean) {
-        throw GradleException("Release build will be performed with not clean git tree; aborting.")
+    if (isRelease) {
+        val grgit = project.findProperty("grgit") as Grgit  // grgit property is added by reckon plugin
+        val isClean = grgit.repository.jgit.status().call().isClean
+        if (!isClean) {
+            throw GradleException("Release build will be performed with not clean git tree; aborting.")
+        }
     }
 }
