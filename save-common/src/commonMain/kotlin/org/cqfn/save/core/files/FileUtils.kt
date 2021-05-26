dce21b92caca790e69670998adca31fc28308058
Peter Trifanov <peter.trifanov@mail.ru>
2021-05-26 12:07:29 +0300

    Reporters API
    ### What's done:
    * Added stdout sink
    * Added save.properties to examples
    * Minor fixes

--- save-common/src/commonMain/kotlin/org/cqfn/save/core/files/FileUtils.kt
+++ save-common/src/commonMain/kotlin/org/cqfn/save/core/files/FileUtils.kt
@@ -2,7 +2,7 @@
  * Utility methods for common operations with file system using okio.
  */
 
-@file:Suppress("FILE_NAME_MATCH_CLASS")
+@file:Suppress("FILE_NAME_MATCH_CLASS", "MatchingDeclarationName")
 
 package org.cqfn.save.core.files
 
