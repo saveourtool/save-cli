f18000f696188fcac423828ce042e84cb7de1157
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-06-09 13:23:58 +0300

    Enable test ### What's done: * enable test

--- save-core/src/commonTest/kotlin/org/cqfn/save/core/PerformAnalysisTest.kt
+++ save-core/src/commonTest/kotlin/org/cqfn/save/core/PerformAnalysisTest.kt
@@ -2,12 +2,10 @@ package org.cqfn.save.core
 
 import org.cqfn.save.core.config.ReportType
 import org.cqfn.save.core.config.SaveProperties
-import kotlin.test.Ignore
 import kotlin.test.Test
 
 class PerformAnalysisTest {
     @Test
-    @Ignore
     fun `detect plugins`() {
         val saveProperties = SaveProperties(
             testRootPath = "../examples",
