d30dc09c83800de9ad89f3b63d6b5a2823476a43
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-15 16:04:30 +0300

    Fixes in warn-plugin
    ### What's done:
    * Fixes

--- examples/kotlin-diktat/run.sh
+++ examples/kotlin-diktat/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #wget -nc https://github.com/pinterest/ktlint/releases/download/0.39.0/ktlint
 wget -nc https://repo1.maven.org/maven2/com/pinterest/ktlint/0.39.0/ktlint-0.39.0.jar
-wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.1/diktat-0.6.1.jar
+wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.2/diktat-0.6.2.jar
 # Download latest release of SAVE CLI
 #wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
 # use this for mingw
@@ -10,5 +10,10 @@ wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.1/diktat-0.6.1.ja
 # cp ../../save-cli/build/bin/linuxX64/releaseExecutable/save-0.1.0-alpha.2.66+20210609T095346Z-linuxX64.exe save
 if [ ! -f save ]; then echo "ERROR: Please place save executable in $PWD before continuing" && exit 1; fi
 chmod +x save
-./save --debug -prop save.properties
-#./save --debug -prop save.properties "warn/MyTest.java" # todo: doesn't work? Uses the whole tree?
+# Run all tests
+#./save --debug -prop save.properties
+# Run only tests in test suite, located in `warn/chapter1`
+#./save --debug -prop save.properties "warn/chapter1/save.toml" # todo: doesn't work now: https://github.com/cqfn/save/issues/137
+# Run single test
+#./save --debug -prop save.properties "warn/chapter1/EnumValueSnakeCaseTest.kt" # todo: currently supports only file name: https://github.com/cqfn/save/issues/137
+./save --debug -prop save.properties "EnumValueSnakeCaseTest.kt"
