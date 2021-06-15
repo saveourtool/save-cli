69adb65e25cbbcbebd7983ba8a1c9962cbe3f7e9
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-15 17:01:53 +0300

    Fixes in warn-plugin (#136)
    ### What's done:
    * warn-plugin: retain file name when creating a copy without technical comments
    * warn-plugin: fixed logic of warnings comparison
    * Added tests

--- save-plugins/warn-plugin/src/commonMain/kotlin/org/cqfn/save/plugin/warn/WarnPlugin.kt
+++ save-plugins/warn-plugin/src/commonMain/kotlin/org/cqfn/save/plugin/warn/WarnPlugin.kt
@@ -11,7 +11,6 @@ import org.cqfn.save.core.result.Fail
 import org.cqfn.save.core.result.Pass
 import org.cqfn.save.core.result.TestResult
 import org.cqfn.save.core.result.TestStatus
-import org.cqfn.save.core.utils.AtomicInt
 import org.cqfn.save.core.utils.ProcessExecutionException
 import org.cqfn.save.plugin.warn.utils.Warning
 import org.cqfn.save.plugin.warn.utils.extractWarning
@@ -19,7 +18,8 @@ import org.cqfn.save.plugin.warn.utils.extractWarning
 import okio.FileSystem
 import okio.Path
 
-private typealias LineColumn = Pair<Int, Int>
+private typealias WarningMap = MutableMap<LineColumn?, List<Warning>>
+internal typealias LineColumn = Pair<Int, Int>
 
 /**
  * A plugin that runs an executable and verifies that it produces required warning messages.
@@ -143,7 +143,7 @@ class WarnPlugin(
 
         createTempDir(tmpDir)
 
-        val fileName = tmpDir / testFileName()
+        val fileName = tmpDir / path.name
         fs.write(fs.createFile(fileName)) {
             fs.readLines(path).forEach {
                 if (!warningsInputPattern.matches(it)) {
@@ -156,12 +156,20 @@ class WarnPlugin(
         return fileName.toString()
     }
 
+    /**
+     * Compares actual and expected warnings and returns TestResult
+     *
+     * @param expectedWarningsMap expected warnings, grouped by LineCol
+     * @param actualWarningsMap actual warnings, grouped by LineCol
+     * @param warnPluginConfig configuration of warn plugin
+     * @return [TestResult]
+     */
     @Suppress("TYPE_ALIAS")
-    private fun checkResults(expectedWarningsMap: Map<LineColumn?, List<Warning>>,
-                             actualWarningsMap: Map<LineColumn?, List<Warning>>,
-                             warnPluginConfig: WarnPluginConfig): TestStatus {
-        val missingWarnings = expectedWarningsMap.filterValues { it !in actualWarningsMap.values }.values
-        val unexpectedWarnings = actualWarningsMap.filterValues { it !in expectedWarningsMap.values }.values
+    internal fun checkResults(expectedWarningsMap: Map<LineColumn?, List<Warning>>,
+                              actualWarningsMap: Map<LineColumn?, List<Warning>>,
+                              warnPluginConfig: WarnPluginConfig): TestStatus {
+        val missingWarnings = expectedWarningsMap.valuesNotIn(actualWarningsMap)
+        val unexpectedWarnings = actualWarningsMap.valuesNotIn(expectedWarningsMap)
 
         return when (missingWarnings.isEmpty() to unexpectedWarnings.isEmpty()) {
             false to true -> Fail("Some warnings were expected but not received: $missingWarnings")
@@ -176,11 +184,14 @@ class WarnPlugin(
             else -> Fail("")
         }
     }
-
-    private fun testFileName(): String = "test_file${atomicInt.addAndGet(1)}"
-
-    companion object {
-        val atomicInt: AtomicInt = AtomicInt(0)
-    }
 }
-typealias WarningMap = MutableMap<Pair<Int, Int>?, List<Warning>>
+
+/**
+ * Collect all values that are present in [this] map, but absent in [other]
+ */
+private fun <K, V> Map<K, List<V>>.valuesNotIn(other: Map<K, List<V>>): List<V> = flatMap { (key, value) ->
+    other[key]?.let { otherValue ->
+        value.filter { it !in otherValue }
+    }
+        ?: value
+}
