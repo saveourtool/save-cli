23f5549224c047944db951618750a1ba5bb41fa2
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-15 15:37:18 +0300

    Fixes in warn-plugin
    ### What's done:
    * Fixes

--- examples/kotlin-diktat/run.sh
+++ examples/kotlin-diktat/run.sh
@@ -2,10 +2,13 @@
 #wget -nc https://github.com/pinterest/ktlint/releases/download/0.39.0/ktlint
 wget -nc https://repo1.maven.org/maven2/com/pinterest/ktlint/0.39.0/ktlint-0.39.0.jar
 wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.1/diktat-0.6.1.jar
-wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
+# Download latest release of SAVE CLI
+#wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
 # use this for mingw
 #wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-mingwX64.exe -O save
-# cp save-0.1.0-alpha.2.66+20210609T095346Z-mingwX64.exe save
+# ... or copy locally built save-cli
+# cp ../../save-cli/build/bin/linuxX64/releaseExecutable/save-0.1.0-alpha.2.66+20210609T095346Z-linuxX64.exe save
+if [ ! -f save ]; then echo "ERROR: Please place save executable in $PWD before continuing" && exit 1; fi
 chmod +x save
 ./save --debug -prop save.properties
 #./save --debug -prop save.properties "warn/MyTest.java" # todo: doesn't work? Uses the whole tree?
