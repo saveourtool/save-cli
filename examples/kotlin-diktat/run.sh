d5afeaf870e80a58e0cb0339c6a84e5e337c59a9
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-18 15:09:01 +0300

    Fixes after review
    ### What's done:
    * Removed quotes in TOMLs
    * Refactored bash script, changing commented out code for conditions

--- examples/kotlin-diktat/run.sh
+++ examples/kotlin-diktat/run.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
-#wget -nc https://github.com/pinterest/ktlint/releases/download/0.39.0/ktlint
-wget -nc https://repo1.maven.org/maven2/com/pinterest/ktlint/0.39.0/ktlint-0.39.0.jar
+
+# Download tools that will be tested
+wget -nc https://github.com/pinterest/ktlint/releases/download/0.39.0/ktlint
 wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.2/diktat-0.6.2.jar
+
 # Download latest release of SAVE CLI
-#wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
-# use this for mingw
-#wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-mingwX64.exe -O save
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-mingwX64.exe -O save
+fi
 # ... or copy locally built save-cli
 # cp ../../save-cli/build/bin/linuxX64/releaseExecutable/save-0.1.0-alpha.2.66+20210609T095346Z-linuxX64.exe save
+
 if [ ! -f save ]; then echo "ERROR: Please place save executable in $PWD before continuing" && exit 1; fi
 chmod +x save
-# Run all tests
-#./save --debug -prop save.properties
-# Run only tests in test suite, located in `warn/chapter1`
-#./save --debug -prop save.properties "warn/chapter1/save.toml" # todo: doesn't work now: https://github.com/cqfn/save/issues/137
-# Run single test
-#./save --debug -prop save.properties "warn/chapter1/EnumValueSnakeCaseTest.kt" # todo: currently supports only file name: https://github.com/cqfn/save/issues/137
-./save --debug -prop save.properties "EnumValueSnakeCaseTest.kt"
+
+if [ "$1" == "--help" ]; then
+    # todo: https://github.com/cqfn/save/issues/137
+    echo 'Usage: ./run.sh [additional SAVE options].
+For example, `./run.sh warn/chapter1/save.toml` to execute tests located in warn/chapter1
+`./run.sh EnumValueSnakeCaseTest.kt to execute a single test`'
+else
+    ./save --debug -prop save.properties $1
+fi
