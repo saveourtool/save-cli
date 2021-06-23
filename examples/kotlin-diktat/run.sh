ad1470677a39f2e29c6bd571df99237f146178c9
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-06-23 16:04:27 +0300

    Refactor script ### What's done: * Refactor script

--- examples/kotlin-diktat/run.sh
+++ examples/kotlin-diktat/run.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-# Download tools that will be tested
-wget -nc https://github.com/pinterest/ktlint/releases/download/0.39.0/ktlint
-wget -nc https://github.com/cqfn/diKTat/releases/download/v0.6.2/diktat-0.6.2.jar
+KTLINT_VERSION=0.39.0
+DIKTAT_VERSION=0.6.2
+SAVE_VERSION=0.1.0-alpha.2
 
-# Download latest release of SAVE CLI
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-linuxX64.kexe -O save
-elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
-    wget -nc https://github.com/cqfn/save/releases/download/v0.1.0-alpha.2/save-0.1.0-alpha.2-mingwX64.exe -O save
+# Download tools that will be tested
+if ! [ -f ktlint ]; then
+  echo "Downloading ktlint-$KTLINT_VERSION"
+  curl -sSLO https://github.com/pinterest/ktlint/releases/download/$KTLINT_VERSION/ktlint
+fi
+
+if ! [ -f diktat-$DIKTAT_VERSION.jar ]; then
+  echo "Downloading diktat-$DIKTAT_VERSION"
+  curl -sSLO https://github.com/cqfn/diKTat/releases/download/v$DIKTAT_VERSION/diktat-$DIKTAT_VERSION.jar
+fi
+
+if ! [ -f save ]; then
+  echo "Downloading SAVE"
+  # Download latest release of SAVE CLI
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+      curl -sSL https://github.com/cqfn/save/releases/download/v$SAVE_VERSION/save-$SAVE_VERSION-linuxX64.kexe -o save
+  elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+      curl -sSL https://github.com/cqfn/save/releases/download/v$SAVE_VERSION/save-$SAVE_VERSION-mingwX64.exe -o save
+  fi
 fi
 # ... or copy locally built save-cli
-# cp ../../save-cli/build/bin/linuxX64/releaseExecutable/save-0.1.0-alpha.2.66+20210609T095346Z-linuxX64.exe save
+# cp ../../save-cli/build/bin/linuxX64/releaseExecutable/save-$SAVE_VERSION.66+20210609T095346Z-linuxX64.exe save
 
 if [ ! -f save ]; then echo "ERROR: Please place save executable in $PWD before continuing" && exit 1; fi
 chmod +x save
