13fc83562be8d958b01c30c7108540fc5391513a
Peter Trifanov <peter.trifanov@mail.ru>
2021-06-30 15:47:47 +0300

    Build optimizations
    ### What's done:
    * Disable redundant KMP targets compilation during build_and_test.yml

---
+++ buildSrc/src/main/kotlin/org/cqfn/save/buildutils/KotlinConfiguration.kt
@@ -0,0 +1,30 @@
+/**
+ * Utility functions to configure Kotlin in Gradle
+ */
+
+package org.cqfn.save.buildutils
+
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+import org.gradle.nativeplatform.platform.internal.DefaultNativePlatform
+
+/**
+ * Some Kotlin Multiplatform targets are available on multiple OS.
+ */
+fun Project.disableRedundantKmpTargets() {
+    if (hasProperty("disableRedundantTargets") && (property("disableRedundantTargets") as String).toBoolean()) {
+        afterEvaluate {
+            val currentOs = DefaultNativePlatform.getCurrentOperatingSystem()
+            val redundantTarget: String? = when {
+                currentOs.isWindows -> "linuxX64"
+                currentOs.isMacOsX -> "linuxX64"
+                currentOs.isLinux -> null
+                else -> throw GradleException("Unknown operating system ${currentOs.name}")
+            }
+            tasks.matching { redundantTarget != null && it.name.contains(redundantTarget, ignoreCase = true) }
+                .configureEach {
+                    enabled = false
+                }
+        }
+    }
+}
