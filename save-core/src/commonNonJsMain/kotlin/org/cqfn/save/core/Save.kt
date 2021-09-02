7397b78e05225300dd2a295705b891844b8a5c53
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-09-02 11:15:04 +0300

    Improve logging in case when nothing were executed (#234)
    ### What's done:
    * Improve logging in case when nothing were executed 
    * Fix examples
    * Bugfix in `makeThePathRelativeToTestRoot` function

--- save-core/src/commonNonJsMain/kotlin/org/cqfn/save/core/Save.kt
+++ save-core/src/commonNonJsMain/kotlin/org/cqfn/save/core/Save.kt
@@ -10,7 +10,6 @@ import org.cqfn.save.core.config.isSaveTomlConfig
 import org.cqfn.save.core.files.ConfigDetector
 import org.cqfn.save.core.files.StdStreamsSink
 import org.cqfn.save.core.files.createRelativePathToTheRoot
-import org.cqfn.save.core.logging.isDebugEnabled
 import org.cqfn.save.core.logging.logDebug
 import org.cqfn.save.core.logging.logError
 import org.cqfn.save.core.logging.logInfo
@@ -36,7 +35,6 @@ import okio.buffer
 /**
  * @property saveProperties an instance of [SaveProperties]
  */
-@Suppress("INLINE_CLASS_CAN_BE_USED")  // todo: remove when there are >1 constructor parameters
 class Save(
     private val saveProperties: SaveProperties,
     private val fs: FileSystem,
@@ -44,16 +42,13 @@ class Save(
     /** reporter that can be used  */
     internal val reporter = getReporter(saveProperties)
 
-    init {
-        isDebugEnabled = saveProperties.debug ?: false
-    }
-
     /**
      * Main entrypoint for SAVE framework. Discovers plugins and calls their execution.
      *
      * @return Reporter
      * @throws PluginException when we receive invalid type of PluginConfig
      */
+    @Suppress("TOO_LONG_FUNCTION")
     fun performAnalysis(): Reporter {
         logInfo("Welcome to SAVE version $SAVE_VERSION")
 
@@ -67,6 +62,7 @@ class Save(
         val excludeSuites = saveProperties.excludeSuites?.split(",") ?: emptyList()
 
         reporter.beforeAll()
+        var atLeastOneExecutionProvided = false
         // get all toml configs in file system
         ConfigDetector(fs)
             .configFromFile(rootTestConfigPath)
@@ -86,6 +82,7 @@ class Save(
                         reporter.onSuiteStart(testConfig.getGeneralConfig()?.suiteName!!)
                     }
                     ?.forEach {
+                        atLeastOneExecutionProvided = true
                         // execute created plugins
                         executePlugin(it, reporter)
                     }
@@ -93,6 +90,16 @@ class Save(
                         reporter.onSuiteEnd(testConfig.getGeneralConfig()?.suiteName!!)
                     }
             }
+        if (!atLeastOneExecutionProvided) {
+            val warnMsg = if (requestedTests.isNotEmpty()) {
+                """|Couldn't found any satisfied test resources for `$requestedTests`
+                   |Please check the correctness of command and consider, that the last arguments treats as test file names for individual execution.
+                """.trimMargin()
+            } else {
+                "SAVE wasn't able to run tests, please check the correctness of configuration and test resources"
+            }
+            logWarn(warnMsg)
+        }
         reporter.afterAll()
         reporter.out.close()
 
