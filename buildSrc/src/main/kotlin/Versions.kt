b30fdb446cbe77fcfc8b41172c7fa59ef1654bc5
Peter Trifanov <peter.trifanov@mail.ru>
2021-09-21 11:12:31 +0300

    Dependency updates (#245)
    ### What's done:
    * Kotlin 1.5.31
    * Diktat 1.0.0-rc.3, code style updates
    * Other dependencies: okio, kotlinx.cli, kotlinx.coroutines, junit

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,16 +1,17 @@
 @file:Suppress("CONSTANT_UPPERCASE")
 
 object Versions {
-    const val kotlin = "1.5.21"
-    const val junit = "5.7.2"
-    const val okio = "3.0.0-alpha.8"
+    const val kotlin = "1.5.31"
+    const val junit = "5.8.0"
+    const val okio = "3.0.0-alpha.9"
     const val ktoml = "0.2.7"
     const val multiplatformDiff = "0.2.0"
 
     object Kotlinx {
         const val serialization = "1.2.2"
         const val datetime = "0.2.1"
-        const val cli = "0.3.2"
+        const val cli = "0.3.3"
+        const val coroutines = "1.5.2"
     }
 
     object IntegrationTest {
