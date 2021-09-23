601fa8d59d7439234c38a63d31520cc67bd7c3d8
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2021-09-23 12:38:11 +0300

    Build fixes
    ### What's done:
    - Regex support

--- save-cli/src/jvmTest/kotlin/org/cqfn/save/cli/GeneralTest.kt
+++ save-cli/src/jvmTest/kotlin/org/cqfn/save/cli/GeneralTest.kt
@@ -16,12 +16,15 @@ import kotlin.test.assertTrue
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
+import org.cqfn.save.core.result.Fail
+import kotlin.test.assertEquals
 
 @Suppress(
     "TOO_LONG_FUNCTION",
     "INLINE_CLASS_CAN_BE_USED",
     "MISSING_KDOC_TOP_LEVEL",
-    "MISSING_KDOC_CLASS_ELEMENTS")
+    "MISSING_KDOC_CLASS_ELEMENTS"
+)
 @OptIn(ExperimentalSerializationApi::class)
 class GeneralTest {
     private val fs = FileSystem.SYSTEM
@@ -81,7 +84,8 @@ class GeneralTest {
             report.pluginExecutions.forEach { pluginExecution ->
                 pluginExecution.testResults.forEach { result ->
                     println(result.status)
-                    assertTrue(result.status is Pass)
+                    result.resources.find { it.name == "ThisShouldAlwaysFailTest.kt" }
+                        ?: assertTrue(result.status is Pass)
                 }
             }
         }
