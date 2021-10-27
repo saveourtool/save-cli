3f41cbf222a7b018c5443a20796416d151a7c975
Peter Trifanov <peter.trifanov@mail.ru>
2021-10-27 16:39:44 +0300

    Build save-common for JS target
    ### What's done:
    * Fix compilation

--- save-core/src/commonNonJsTest/kotlin/org/cqfn/save/core/integration/WarnDirTest.kt
+++ save-core/src/commonNonJsTest/kotlin/org/cqfn/save/core/integration/WarnDirTest.kt
@@ -10,14 +10,17 @@ import kotlin.test.Test
 
 class WarnDirTest {
     @Test
-    fun `execute warn plugin on the directory chapter1`() =
-            runTestsWithDiktat(listOf("warn-dir/chapter1"), 3)
+    fun `execute warn plugin on the directory chapter1`() {
+        runTestsWithDiktat(listOf("warn-dir/chapter1"), 3)
+    }
 
     @Test
-    fun `execute warn plugin on the directory chapter2`() =
-            runTestsWithDiktat(listOf("warn-dir/chapter2"), 1)
+    fun `execute warn plugin on the directory chapter2`() {
+        runTestsWithDiktat(listOf("warn-dir/chapter2"), 1)
+    }
 
     @Test
-    fun `execute warn plugin on the directory chapter3`() =
-            runTestsWithDiktat(listOf("warn-dir/chapter3"), 1)
+    fun `execute warn plugin on the directory chapter3`() {
+        runTestsWithDiktat(listOf("warn-dir/chapter3"), 1)
+    }
 }
