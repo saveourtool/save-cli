ec423e121bb8e950f60c6018a0b8d836e12f4b3f
Peter Trifanov <peter.trifanov@mail.ru>
2021-11-15 18:37:53 +0300

    Remove redundant stuff from build_and_test.yml
    ### What's done:
    * Don't install curl

--- save-core/src/commonNonJsTest/kotlin/org/cqfn/save/core/integration/ClassicWarnTest.kt
+++ save-core/src/commonNonJsTest/kotlin/org/cqfn/save/core/integration/ClassicWarnTest.kt
@@ -5,10 +5,6 @@ import org.cqfn.save.core.test.utils.runTestsWithDiktat
 import kotlin.test.Ignore
 import kotlin.test.Test
 
-// To run these tests locally on your Native platforms you would need to install curl for your OS:
-// On Windows you'll also need to install msys2 and run pacman -S mingw-w64-x86_64-curl to have libcurl for ktor-client.
-// On ubuntu install libcurl4-openssl-dev for ktor client and libncurses5 for kotlin/native compiler.
-
 /**
  * testing that save can successfully work with:
  * 1) separate files
