5bcadaeb026843366a89c86fd1047fa533a2112b
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2021-11-19 17:25:31 +0300

    WIP ### What's done: * WIP

--- examples/kotlin-diktat/warn/chapter1/TestMatchOnlyByLineColumn/EnumValueSnakeCaseTest.kt
+++ examples/kotlin-diktat/warn/chapter1/TestMatchOnlyByLineColumn/EnumValueSnakeCaseTest.kt
@@ -1,15 +1,15 @@
 package org.cqfn.diktat.test.resources.test.paragraph1.naming.enum_
 
-// ;warn:3:1: a
-// ;warn:35: a
-// ;warn:10:5: This message will be ignored anyway, don't matter what this line contain+
+// ;warn:3:1
+// ;warn:6:35
+// ;warn:10:5: This message will be ignored anyway, don't matter what this line contain
 enum class EnumValueSnakeCaseTest {
-    // ;warn:$line+1:5: s
+    // ;warn:$line+1:5
     paSC_SAl_l,
 
-    // ;warn:5: a
+    // ;warn:5
     PascAsl_f
-    // ;warn:$line-2:5: a+
+    // ;warn:$line-2:5
 
-    // ;warn:1:9: a+
+    // ;warn:1:9
 }
