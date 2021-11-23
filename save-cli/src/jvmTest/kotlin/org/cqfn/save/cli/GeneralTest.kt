a87a20f6e98425c68f9b1b31f1317add2ff17dc2
sanyavertolet <alexander.frolov@huawei.com>
2021-11-23 15:36:08 +0300

    renovate/core-kotlin
    ###What's done:
     * TESTING
    (#317)

--- save-cli/src/jvmTest/kotlin/org/cqfn/save/cli/GeneralTest.kt
+++ save-cli/src/jvmTest/kotlin/org/cqfn/save/cli/GeneralTest.kt
@@ -75,7 +75,7 @@ class GeneralTest {
         val saveFlags = " . --result-output FILE --report-type JSON"
         // Execute the script from examples
         val execCmd = "$runCmd$saveBinName $saveFlags"
-        val pb = ProcessBuilder(true, fs).exec(execCmd, examplesDir, null, 100_000L)
+        val pb = ProcessBuilder(true, fs).exec(execCmd, examplesDir, null, 300_000L)
         println("SAVE execution output:\n${pb.stdout.joinToString("\n")}")
         if (pb.stderr.isNotEmpty()) {
             println("Warning and errors during SAVE execution:\n${pb.stderr.joinToString("\n")}")
