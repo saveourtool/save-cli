c1375abc30f9926d50a509887a7cd9144c87dcfc
Andrey Kuleshov <andrewkuleshov7@gmail.com>
2022-01-24 16:07:25 +0300

    Supporting Sarif format for Actual messages

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,9 +2,9 @@
 
 object Versions {
     object IntegrationTest {
-        const val ktlint = "0.39.0"
+        const val ktlint = "0.43.2"
         const val ktlintLink = "https://github.com/pinterest/ktlint/releases/download/$ktlint/ktlint"
-        const val diktat = "1.0.0-rc.2"
+        const val diktat = "1.0.2"
         const val diktatLink = "https://github.com/cqfn/diKTat/releases/download/v$diktat/diktat-$diktat.jar"
     }
 }
