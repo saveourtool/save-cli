27c7157ae893876fcf2a67afa0bd6c2895297e9d
Vfrolov <vlad-20-11-94@yandex.ru>
2022-05-24 12:44:05 +0300

    Parsing exceptions during the parsing of invalid SARIF result
    * Added exception for parsing sarif
    Closes #388

---
+++ save-common/src/commonMain/kotlin/org/cqfn/save/core/utils/ProcessParseSarifException.kt
@@ -0,0 +1,6 @@
+package org.cqfn.save.core.utils
+
+/**
+ * An [Exception] that can be thrown in case of failed to parse sarif
+ */
+open class ProcessParseSarifException(message: String) : Exception(message)
