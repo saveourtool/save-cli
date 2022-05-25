274ab3d1d2eeaac06d643164d160083ffc018a98
Peter Trifanov <peter.trifanov@mail.ru>
2022-05-25 11:39:14 +0300

    Fix URLs for publications to sonatype (#392)

--- buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/PublishingConfiguration.kt
+++ buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/PublishingConfiguration.kt
@@ -139,8 +139,8 @@ private fun Project.configureNexusPublishing() {
     configure<NexusPublishExtension> {
         repositories {
             sonatype {
-                nexusUrl.set(uri("https://oss.sonatype.org/service/local/"))
-                snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
+                nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+                snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
                 username.set(property("sonatypeUsername") as String)
                 password.set(property("sonatypePassword") as String)
             }
