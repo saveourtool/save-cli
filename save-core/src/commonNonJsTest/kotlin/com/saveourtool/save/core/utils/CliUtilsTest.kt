0fdfff0aa1bb700633d9f311abd29663318995eb
Nariman Abdullin <nulls.narik@gmail.com>
2022-06-27 19:32:10 +0300

    added tests for util classes

---
+++ save-core/src/commonNonJsTest/kotlin/com/saveourtool/save/core/utils/CliUtilsTest.kt
@@ -0,0 +1,91 @@
+package com.saveourtool.save.core.utils
+
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import okio.FileSystem
+import okio.IOException
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class CliUtilsTest {
+    private val fs: FileSystem = FileSystem.SYSTEM
+    private val tmpDir = (FileSystem.SYSTEM_TEMPORARY_DIRECTORY / FileSystem::class.simpleName!!)
+
+    @Test
+    fun `read properties file`() {
+        val testFile = tmpDir / "test.properties"
+        fs.write(testFile) {
+            this.write("key1=value1\n".encodeToByteArray())
+            this.write("key2=value2\n".encodeToByteArray())
+        }
+
+        assertEquals(
+            TestProperties("value1", "value2"),
+            CliUtils.parsePropertiesFile(fs,
+                tmpDir.toString(), "test")
+        )
+    }
+
+    @Test
+    fun `read properties file from not existed folder`() {
+        assertFailsWith<IOException> {
+            CliUtils.parsePropertiesFile<TestProperties>(fs,
+                tmpDir.resolve("not-existed").toString(), "test")
+        }
+    }
+
+    @Test
+    fun `resolve value`() {
+        val invalidParser = ArgParser("invalid")
+        val parser = ArgParser("test")
+        val key1Option = parser.option(ArgType.String, "key1").default("default_value1")
+        val key1 by key1Option
+        val key2Option = parser.option(ArgType.String, "key2").default("default_value2")
+        val key2 by key2Option
+        val key3Option = invalidParser.option(ArgType.String, "key3").default("default_value3")
+        val key3 by key3Option
+
+        parser.parse(listOf("--key1", "some_value1").toTypedArray())
+
+        assertEquals("some_value1", key1)
+        assertEquals(
+            "cli_value1",
+            CliUtils.resolveValue(key1Option, "cli_value1", "override_value1")
+        )
+
+        assertEquals("default_value2", key2)
+        assertEquals(
+            "override_value2",
+            CliUtils.resolveValue(key2Option, "cli_value2", "override_value2")
+        )
+
+        assertFailsWith<RuntimeException> {
+            // touch variable
+            key3.length
+        }
+        assertEquals(
+            "override_value3",
+            CliUtils.resolveValue(key3Option, "cli_value3", "override_value3")
+        )
+    }
+
+    @BeforeTest
+    fun createTmpFolder() {
+        fs.createDirectory(tmpDir)
+    }
+
+    @AfterTest
+    fun cleanupTmpFolder() {
+        fs.deleteRecursively(tmpDir)
+    }
+
+    @kotlinx.serialization.Serializable
+    private data class TestProperties(
+        val key1: String,
+        val key2: String
+    )
+}
