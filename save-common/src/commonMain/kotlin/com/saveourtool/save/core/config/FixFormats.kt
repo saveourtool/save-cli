118317c6d26a267c9c823c5e2f22a681a5d3a219
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2022-11-29 17:51:24 +0300

    Codestyle ### What's done: * codestyle

--- save-common/src/commonMain/kotlin/com/saveourtool/save/core/config/FixFormats.kt
+++ save-common/src/commonMain/kotlin/com/saveourtool/save/core/config/FixFormats.kt
@@ -2,7 +2,7 @@
  * Classes that describe format of expected and actual fixes
  */
 
-@file:Suppress("FILE_NAME_MATCH_CLASS")
+@file:Suppress("FILE_NAME_MATCH_CLASS", "MatchingDeclarationName")
 
 package com.saveourtool.save.core.config
 
