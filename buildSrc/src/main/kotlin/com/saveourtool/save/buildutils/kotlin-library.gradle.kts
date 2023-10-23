114e6e981490832eaad2ba18d0fa59a94a13ee96
Kirill Gevorkyan <26010098+kgevorkyan@users.noreply.github.com>
2023-10-23 13:09:20 +0300

    Update to kotlin 1.9.10 (#544)

--- buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/kotlin-library.gradle.kts
+++ buildSrc/src/main/kotlin/com/saveourtool/save/buildutils/kotlin-library.gradle.kts
@@ -69,9 +69,6 @@ kotlin {
      * linux  mingw macos
      */
     sourceSets {
-        all {
-            languageSettings.optIn("kotlin.RequiresOptIn")
-        }
         val commonMain by getting
         val commonTest by getting {
             dependencies {
--- save-common/src/nativeMain/kotlin/com/saveourtool/save/core/files/FileUtils.kt
+++ save-common/src/nativeMain/kotlin/com/saveourtool/save/core/files/FileUtils.kt
@@ -9,14 +9,10 @@ import com.saveourtool.save.core.logging.logTrace
 import okio.FileSystem
 import okio.Path
 import okio.Path.Companion.toPath
-import platform.posix.FTW
 import platform.posix.FTW_DEPTH
 import platform.posix.nftw
 import platform.posix.remove
-import platform.posix.stat
 
-import kotlinx.cinterop.ByteVar
-import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.staticCFunction
 import kotlinx.cinterop.toKString
 
@@ -29,7 +25,8 @@ actual val fs: FileSystem = FileSystem.SYSTEM
  */
 @Suppress("MAGIC_NUMBER", "MagicNumber")
 actual fun FileSystem.myDeleteRecursively(path: Path) {
-    nftw(path.toString(), staticCFunction<CPointer<ByteVar>?, CPointer<stat>?, Int, CPointer<FTW>?, Int> { pathName, _, _, _ ->
+    @OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+    nftw(path.toString(), staticCFunction { pathName, _, _, _ ->
         val fileName = pathName!!.toKString()
         logTrace("Attempt to delete file $fileName")
         remove(fileName)
